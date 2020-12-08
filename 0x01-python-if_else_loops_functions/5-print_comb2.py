ff0610147c9e3962900fe6f335407daafb161d2f
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-08 14:53:39 +0000

    python loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0", end='')
     if i == 99:
         print("{:d}".format(i))
     else:
-        print("{:d}, ".format(i), end='')
+        print("{:02d}, ".format(i), end='')

9ce1cb083cbb0d301ef52acf918f373ac9819172
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-10 14:17:36 +0000

    import module python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
-result = 0
-for i in range(1, n):
-    result = result + int(sys.argv[i])
-
-print(result)
+if __name__ == "__main__":
+    def main():
+        import sys
+        n = len(sys.argv)
+        result = 0
+        for i in range(1, n):
+            result = result + int(sys.argv[i])
+        print(result)
+    main()

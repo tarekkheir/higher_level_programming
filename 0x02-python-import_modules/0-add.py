feb6dcaa2c7920d2e140d93018b2d14c95051ab8
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-10 13:50:55 +0000

    import module python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    def main():
-        from add_0 import add
-        a = 1
-        b = 2
-        print("{} + {} = {}".format(a, b, add(1, 2)))
-
-    main()
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(1, 2)))

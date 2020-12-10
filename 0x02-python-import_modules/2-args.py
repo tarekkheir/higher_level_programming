86149e33deca41a34c67423a49406742efb69c2a
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-10 20:45:27 +0000

    import module python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     n = 0
     n = len(sys.argv)
     if n == 1:
-        print("{} arguments".format(n - 1))
+        print("{} arguments:".format(n - 1))
     if n == 2:
-        print("{} argument".format(n - 1))
+        print("{} argument:".format(n - 1))
     if n > 2:
-        print("{} arguments".format(n - 1))
+        print("{} arguments:".format(n - 1))
         for i in range(1, n):
             print("{}: {}".format(i, sys.argv[i]))

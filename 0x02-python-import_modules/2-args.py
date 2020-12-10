88cad37878e2001b6ea37bd99c71ff4ac31974b9
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-10 20:52:31 +0000

    import module python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     if n == 1:
         print("{} arguments:".format(n - 1))
     if n == 2:
-        print("{} argument:".format(n - 1))
+        print("{} argument:".format(n - 1), end=' ')
+        print(sys.argv[1])
     if n > 2:
         print("{} arguments:".format(n - 1))
         for i in range(1, n):

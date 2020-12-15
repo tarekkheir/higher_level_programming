70fa40a2b11d9c9b552d39c57f30a1f7816f3bb5
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-15 15:02:55 +0000

    python data structure

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if len(my_list) < idx:
+    if len(my_list) <= idx:
         return None
     elif idx < 0:
         return None
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if len(my_list) < idx:
+    if len(my_list) <= idx:
         return my_list.copy()
     elif idx < 0:
         return my_list.copy()

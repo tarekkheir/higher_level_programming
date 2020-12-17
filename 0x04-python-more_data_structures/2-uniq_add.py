2985c5196f7300affd8b8928b863aa2c23b9846e
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-17 20:48:03 +0000

    python-more_data_structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    new = [0]
+    count = 0
+    present = 0
     for i in range(0, len(my_list)):
-        count = my_list.count(my_list[i])
-        if count != 1:
-            my_list.pop(i)
-
-    for i in range(0, len(my_list)):
-        result += my_list[i]
-    return result
+        for j in range(0, len(new)):
+            if my_list[i] == new[j]:
+                present += 1
+        if present == 0:
+            new.append(my_list[i])
+        else:
+            present = 0
+    for i in range(0, len(new)):
+        count += new[i]
+    return count

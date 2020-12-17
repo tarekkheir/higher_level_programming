87d59282e061c69e4bb67fbedfd1f6f7fb53e83d
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-17 22:51:59 +0000

    python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary == None or a_dictionary == "":
         return None
     new = sorted(a_dictionary)
     result = 0

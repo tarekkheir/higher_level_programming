34839b12dfac725deb62028150f5f57761176877
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-17 20:57:46 +0000

    python-more_data_structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    new = sorted(a_dictionary)
+    result = 0
+    key = ""
+    for i in range(0, len(a_dictionary)):
+        if a_dictionary[new[i]] > result:
+            result = a_dictionary[new[i]]
+            key = new[i]
+    return key

80a08ae5c265a4659bdd20b4bafd1f2999b7bec2
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-17 20:59:38 +0000

    python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,6 @@ def best_score(a_dictionary):
         return None
     new = sorted(a_dictionary)
     result = 0
-    key = ""
     for i in range(0, len(a_dictionary)):
         if a_dictionary[new[i]] > result:
             result = a_dictionary[new[i]]

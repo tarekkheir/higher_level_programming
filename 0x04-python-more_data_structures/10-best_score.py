e06202ca4c8002de6731312e2a215f7eadd96f35
Tarek Kheir <kheirtarek@hotmail.fr>
2020-12-18 13:14:31 +0000

    python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == { }:
+    if a_dictionary == { } or a_dictionary is None:
         return None
     result = 0
     key = 0

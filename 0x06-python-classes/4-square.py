cead3df05f5441ea6b7ebf10bb78621288b5e435
tarekkheir <kheirtarek@hotmail.fr>
2021-01-06 00:32:06 +0100

    Python classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Square module - init
+"""
+
+
+class Square:
+    """
+    Square class:
+
+    Attributes: size
+    """
+    def __init__(self, size=None):
+        """
+        Square init: square parameters
+        """
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
+
+    def size(self):
+        return self.size
+
+    def size(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = value
+
+    def area(self):
+        """
+        Method return area
+        """
+        if isinstance(self.size, int) is True:
+            return (self.size * self.size)
+        else:
+            raise TypeError("size must be an integer")

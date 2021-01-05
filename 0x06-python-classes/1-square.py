c840741f4d56d246000babfef525e7efd79b87f6
tarekkheir <kheirtarek@hotmail.fr>
2021-01-05 17:18:08 +0100

    Python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square module - init two instances
+"""
+
+
+class Square:
+    """
+    Square class:
+    
+    Attributes:
+    size
+    """
+    def __init__(self, size):
+        self.__size = size
+    pass

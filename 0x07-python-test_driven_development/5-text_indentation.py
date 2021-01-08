0c0b68b130c3c08fa19e3d4d852dd36ed1c6c665
tarekkheir <kheirtarek@hotmail.fr>
2021-01-08 22:39:50 +0100

    Python test driven development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-
+"""
+Module - text indentation
+Method: text_indentation
+Attribute: text
+"""
 def text_indentation(text):
+    """
+    print text by line
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
 

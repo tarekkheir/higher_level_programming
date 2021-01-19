4f83bd38d3a512d6130f414f630459c27b013451
Tarek <kheirtarek@hotmail.fr>
2021-01-19 21:45:19 +0100

    Python inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,18 +7,14 @@ MyInt module - task 100
 class MyInt(int):
     """ Class MyInt """
 
-    def __init__(self, state):
-        """ Init execute"""
-        self.var = state
-
     def __ne__(self, other):
         """not equal method"""
-        if self.a != other:
+        if self.var != other:
             return False
         return True
 
     def __eq__(self, other):
         """equal method"""
-        if self.a == other:
+        if self.var == other:
             return False
         return True

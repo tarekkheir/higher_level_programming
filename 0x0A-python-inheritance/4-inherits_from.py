156215822ba4c5f3f79e035a0de31e6023bb0d97
Tarek <kheirtarek@hotmail.fr>
2021-01-19 18:37:21 +0100

    Python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,5 +9,5 @@ def inherits_from(obj, a_class):
     that inherited from the specified class
     """
     if type(obj) == a_class:
-        return Falsen
+        return False
     return isinstance(obj, a_class)

93577b90729ce48c52f64ff4671963d1ef49e259
Tarek <kheirtarek@hotmail.fr>
2021-01-20 20:35:25 +0100

    Python I/O

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,26 +4,28 @@ I/O module - task 7
 """
 
 
-import json
 import sys
 import os
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-fp = open(filename, "r+")
+if __name__ == "__main__":
+    """ main code executed"""
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if os.path.getsize(filename) > 0:
+    filename = "add_item.json"
+    fp = open(filename, "r+")
 
-    if len(sys.argv) > 1:
+    if os.path.getsize(filename) > 0:
 
-        data = load_from_json_file(filename)
-        for i in range(1, len(sys.argv)):
-            data.append(sys.argv[i])
-        save_to_json_file(data, filename)
-else:
-    new_list = []
-    save_to_json_file(new_list, filename)
+        if len(sys.argv) > 1:
 
-fp.close()
+            data = load_from_json_file(filename)
+            for i in range(1, len(sys.argv)):
+                data.append(sys.argv[i])
+            save_to_json_file(data, filename)
+    else:
+        new_list = []
+        save_to_json_file(new_list, filename)
+
+    fp.close()

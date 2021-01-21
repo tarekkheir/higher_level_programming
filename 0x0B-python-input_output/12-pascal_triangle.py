6dbd33a760bc591e97d02529098f508bfbe790b8
Tarek <kheirtarek@hotmail.fr>
2021-01-21 15:58:32 +0100

    Python input output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,6 +4,16 @@ I/O module - task 12
 """
 
 
+def factoriel(n):
+    if n == 0:
+        return 1
+    else:
+        nb = 1
+        for i in range(2,n+1):
+            nb = nb * i
+        return nb
+
+
 def pascal_triangle(n):
     """returns a list of lists of
     integers representing the Pascal’s triangle of n"""
@@ -13,20 +23,18 @@ def pascal_triangle(n):
     if n <= 0:
         return lists
 
-    for i in range(1, n + 1):
+    for i in range(0, n):
 
         new_list = []
-
+        if i == 0:
+            new_list.append(1)
         for j in range(0, i):
-            if j == 0 or j == i - 1:
-                new_list.append(1)
-            elif j == 1:
-                new_list.append(i - 1)
-            else:
-                if lists[i - 2][j] == 1:
-                    new_list.append(lists[i - 2][j] + j)
-                else:
-                    new_list.append(lists[i - 2][j] + j + 1)
+
+            nb = factoriel(i) / (factoriel(i - j) * factoriel(j))
+            new_list.append(int(nb))
+
+        if i != 0:
+            new_list.append(1)
         lists.append(new_list)
 
     return lists

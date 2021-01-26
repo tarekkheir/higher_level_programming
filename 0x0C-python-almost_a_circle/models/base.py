16b5722f8f7fba8aa5a797ca16b7e0c6a285e8a9
Tarek <kheirtarek@hotmail.fr>
2021-01-26 22:15:34 +0100

    fix base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,7 +31,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
 
-        if json_string is None or not json_string:
+        if json_string is None or json_string == string.empty:
             json_string = []
         return json.loads(json_string)
 

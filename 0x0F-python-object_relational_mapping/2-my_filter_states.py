0c61fb2547ba03f6a7bf34e45bceb3321e9ac702
Tarek <kheirtarek@hotmail.fr>
2021-03-28 21:44:03 +0200

    fix 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,8 +4,6 @@ script that takes in an argument and displays
 all values in the states table of hbtn_0e_0_usa
 where name matches the argument
 """
-
-
 if __name__ == "__main__":
 
     import MySQLdb

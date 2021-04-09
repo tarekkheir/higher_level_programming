20bb7ee3b9ad9d7b7e6c6e5ec00689e54d271091
Tarek <kheirtarek@hotmail.fr>
2021-04-09 16:22:56 +0200

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def find_peak(list_of_integers):
+    n = len(list_of_integers)
+    if n == 0:
+        return None
+    if n == 1:
+        return list_of_integers[0]
+    if (list_of_integers[0] >= list_of_integers[1]) :
+        return 0
+    if (list_of_integers[n - 1] >= list_of_integers[n - 2]) :
+        return n - 1
+
+    for i in range(1, n - 1):
+        if (list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]) :
+            return i

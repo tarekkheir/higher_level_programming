0f84e73eccea917fca9e3274a9724e3190a820b3
Tarek <kheirtarek@hotmail.fr>
2021-04-09 23:22:35 +0200

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,10 +2,14 @@
 """ Python script that fetches https://intranet.hbtn.io/status"""
 
 
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
+from urllib import request
 
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
+
+if __name__ == "__main__":
+
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))

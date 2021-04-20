7b8b30663b0c928b1c3fe2455fa1f9b3643509a8
Tarek <kheirtarek@hotmail.fr>
2021-04-20 07:33:45 +0200

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL and displays the body of the response."""
+
+
+import requests
+import sys
+
+
+def main(url):
+
+    resp = requests.get(url)
+
+    if resp.status_code >= 400:
+        print("Error code: {}".format(resp.status_code))
+    else:
+        print(resp.text)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    main(url)

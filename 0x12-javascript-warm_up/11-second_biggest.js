a69f73ae6657aded6ae3008c36789d030568cb5e
Tarek <kheirtarek@hotmail.fr>
2021-04-20 07:57:02 +0200

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const arr = process.argv.slice(2).map(Number);
 if (arr.length <= 1) {
   console.log(0);
 } else {
-  var max = Math.max.apply(null, arr);
+  const max = Math.max.apply(null, arr);
   console.log(max)
 }

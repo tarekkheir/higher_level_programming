3af7db255116f3a2252e8a4829f44888821f49ce
Tarek <kheirtarek@hotmail.fr>
2021-04-20 08:01:07 +0200

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,5 @@ if (arr.length <= 1) {
   console.log(0);
 } else {
   arr.splice(arr.indexOf(Math.max.apply(null, arr)), 1);
-  console.log(Math.max.apply(null, arr))
+  console.log(Math.max.apply(null, arr));
 }

d9b5a2191a85ac1aaecbaee128115a78dd1c0ece
Tarek <kheirtarek@hotmail.fr>
2021-04-20 07:59:53 +0200

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const arr = process.argv.slice(2).map(Number);
 if (arr.length <= 1) {
   console.log(0);
 } else {
-  const max = Math.max.apply(null, arr);
-  console.log(max)
+  arr.splice(arr.indexOf(Math.max.apply(null, arr)), 1);
+  console.log(Math.max.apply(null, arr))
 }
